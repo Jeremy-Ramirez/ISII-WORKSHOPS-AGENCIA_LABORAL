47f2f4a7e41665d22a70405ef1c2aeff0082060b
diacrami <diacrami@espol.edu.ec>
2021-10-18 15:09:36 -0500

    Creation of variables in Class CarInsurance

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,6 +1,10 @@
 package ec.edu.espol.workshops;
 
 public class CarInsurance {
+	public int age;
+	public char sex;
+	public String maritalStatus;
+	final int basePremium=500;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
