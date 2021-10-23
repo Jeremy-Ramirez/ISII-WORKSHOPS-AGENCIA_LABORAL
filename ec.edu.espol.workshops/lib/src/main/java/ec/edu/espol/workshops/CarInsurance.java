3a0f0c94b078ce59765ce8ae6c82cabea8268db2
diacrami <diacrami@espol.edu.ec>
2021-10-22 20:10:48 -0500

    Violations solved - Checkstyle

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -4,87 +4,96 @@ import java.util.Scanner;
 
 public class CarInsurance {
 	
-	public static class Customer{
-		public int age;
-		public String sex;
-		public String maritalStatus;
-		public boolean hasLicence;
+  public static class Customer{
+    public int age; 
+    public String sex;
+    public String maritalStatus;
+    public boolean hasLicence;
 		
-		public Customer(int age, String sex, String maritalStatus, boolean hasLicence) {
-			//super();
-			this.age = age;
-			this.sex = sex;
-			this.maritalStatus = maritalStatus;
-			this.hasLicence = hasLicence;
-		}
+    public Customer(int age, String sex, String maritalStatus, boolean hasLicence) {
+      this.age = age;
+	  this.sex = sex;
+	  this.maritalStatus = maritalStatus;
+	  this.hasLicence = hasLicence;
+    }
 
-		public int getAge() {
-			return age;
-		}
-		public String getSex() {
-			return sex;
-		}
+    public int getAge() {
+	  return age;
+    }
+    public String getSex() {
+	  return sex;
+    }
 
-		public String getMaritalStatus() {
-			return maritalStatus;
-		}
-
-		
-		public boolean isHasLicence() {
-			return hasLicence;
-		}
-
-		public void setHasLicence(boolean hasLicence) {
-			this.hasLicence = hasLicence;
-		}
+    public String getMaritalStatus() {
+	  return maritalStatus;
+    }
 
 		
+    public boolean isHasLicence() {
+	  return hasLicence;
+    }
+
+    public void setHasLicence(boolean hasLicence) {
+	  this.hasLicence = hasLicence;
+    }
+
 		
-		public void  setAge(int age)    { 
-	            this.age = age;
-	    }
-		public void  setSex(String sex)    {
-	        this.sex = sex;
-		}
-		public void  setMaritalStatus(String maritalStatus)    {
-	        this.maritalStatus = maritalStatus;
-		}
-	}
+		
+    public void  setAge(int age)    { 
+	  this.age = age;
+    }
+    public void  setSex(String sex)    {
+	  this.sex = sex;
+    }
+    public void  setMaritalStatus(String maritalStatus)    {
+	  this.maritalStatus = maritalStatus;
+    }
+  }
 	
+  /**
+   * The base insurance cannot be change
+  */
+  final static int basePremium=500; 
 	
+  /**
+   * 
+  */
+  public double computePremium(Customer c) {
+    int premium = 0;
+    if (c.getSex() == "M" && c.getMaritalStatus().equals("not married") && c.getAge() < 25) {
+      premium = basePremium + 1500;
+      return premium;
+    }
+    if (c.getSex() == "F" || c.getMaritalStatus().equals("married")) {
+      premium = basePremium - 200;
+      return premium;
+    }
+    if (c.getAge() >= 45 && c.getAge() <= 65) {
+      premium = basePremium - 100;
+      return premium;
+    }
+    return basePremium;
+    }
 	
-	final int basePremium=500;
-	
-	public double computePremium(Customer c) {
-		int premium=0;
-		if(c.getSex()=="M" && c.getMaritalStatus().equals("not married") && c.getAge()<25) {
-			premium = basePremium + 1500;
-			return premium;
-		}
-		if(c.getSex()=="F" || c.getMaritalStatus().equals("married")) {
-			premium = basePremium -200;
-			return premium;
-		}
-		if(c.getAge()>=45 && c.getAge()<=65) {
-			premium = basePremium - 100;
-			return premium;
-		}
-		return basePremium;
-	}
-	
+	/**
+	 * 
+	*/
 	public String policies(Customer c) {
 		if(c.getAge()>80 || !(c.isHasLicence()) ) {
 			return "not sell";
 		}
 		return "sell";
 	}
-
+	
+	/**
+	 * 
+	*/
 	public static void main(String[] args) {
 			
-			Customer c1 = new Customer(23, "M", "married", false);
-			CarInsurance car= new CarInsurance();
-			System.out.println(car.computePremium(c1));
-			System.out.println(car.policies(c1));
+		Customer c1 = new Customer(23, "M", "married", false);
+		CarInsurance car= new CarInsurance();
+		System.out.println(car.computePremium(c1));
+		System.out.println(car.policies(c1));
 
 	}
 
