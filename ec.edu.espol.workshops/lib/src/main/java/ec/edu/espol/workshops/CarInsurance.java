90c1600a2c89b57cc0ddc6810eb970573fe7a3ae
diacrami <diacrami@espol.edu.ec>
2021-11-19 20:25:37 -0500

    Tested Code

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -106,21 +106,43 @@ public class CarInsurance {
 	*/
 	public static void main(String[] args) {
 		
-		Customer c1 = new Customer(15, "M", "not married", false);
-		Customer c2 = new Customer(36, "F", "married", true);
-		Customer c3 = new Customer(19, "A", "married", false);
-		Customer c4 = new Customer(81, "F", "not married", true);
-		//Customer c5 = new Customer(13.5, "M", "married", falso);
-		Customer c6 = new Customer(25, "M", "casado", false);
+		Customer c1 = new Customer(18, "F", "not married", false);
+		Customer c2 = new Customer(17, "F", "married", false);
+		Customer c3 = new Customer(19, "M", "married", true);
+		Customer c4 = new Customer(25, "M", "married", true);
+		Customer c5 = new Customer(24, "M", "married", true);
+		
+		Customer c6 = new Customer(26, "M", "married", false);
+		Customer c7 = new Customer(45, "M", "married", false);
+		Customer c8 = new Customer(44, "M", "not married", false);
+		Customer c9 = new Customer(46, "M", "married", false);
+		Customer c10 = new Customer(65, "M", "married", true);
+		
+		Customer c11 = new Customer(64, "M", "not married", false);
+		Customer c12 = new Customer(66, "M", "married", false);
+		Customer c13 = new Customer(80, "M", "not married", true);
+		Customer c14 = new Customer(79, "M", "married", false);
+		Customer c15 = new Customer(81, "F", "married", true);
+
 		
 		
 		CarInsurance car= new CarInsurance();
 		
-		System.out.println(car.computePremium(c1));
-		System.out.println(car.computePremium(c2));
-		System.out.println(car.computePremium(c3));
-		System.out.println(car.policies(c4));
-		System.out.println(car.computePremium(c6));
+		System.out.println("c1 " + car.computePremium(c1));
+		System.out.println("c2 " + car.computePremium(c2));
+		System.out.println("c3 " + car.computePremium(c3));
+		System.out.println("c4 " + car.computePremium(c4));
+		System.out.println("c5 " + car.computePremium(c5));
+		System.out.println("c6 " + car.computePremium(c6));
+		System.out.println("c7 " + car.computePremium(c7));
+		System.out.println("c8 " + car.computePremium(c8));
+		System.out.println("c9 " + car.computePremium(c9));
+		System.out.println("c10 " + car.computePremium(c10));
+		System.out.println("c11 " + car.computePremium(c11));
+		System.out.println("c12 " + car.computePremium(c12));
+		System.out.println("c13 " + car.computePremium(c13));
+		System.out.println("c14 " + car.computePremium(c14));
+		System.out.println("c15 " + car.computePremium(c15));
 
 	}
 
